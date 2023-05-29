d64b67b5203b9f890405494495c7c2b33e8567a7
Jonah Pickett <jpickettpre@gmail.com>
2023-05-29 17:15:09 -0400

    Initial Commit

---
+++ Alt Vault/Alt_VaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alt_VaultApp.swift
+//  Alt Vault
+//
+//  Created by Jonah Pickett on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Alt_VaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
